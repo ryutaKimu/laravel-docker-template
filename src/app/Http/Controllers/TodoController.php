80596747b6b402e9742f6fd4b54bf324785dcd87
ryutaKimu <rekkoilove@softbank.ne.jp>
2024-07-20 23:39:21 +0900

    add:todosのmodel追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
-    public function index(){
-        return view('todo.index');
-    }
+  public function index()
+  {
+    $todo = new Todo();
+    $todos = $todo->all();
+    dd($todos);
+    return view('todo.index');
+  }
 }
