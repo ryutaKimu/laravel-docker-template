89456470172eae643412c773a8889f6c1ab96cc7
ryutaKimu <rekkoilove@softbank.ne.jp>
2024-07-21 02:26:08 +0900

    fix: storeのルーティングのpathを修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -20,4 +20,4 @@ Route::get('/', function () {
 
 Route::get('/todo','TodoController@index')->name('todo.index');
 Route::get('/todo/create','TodoController@create')->name('todo.create');
-Route::post('/todo/store','TodoController@store')->name('todo.store');
+Route::post('/todo','TodoController@store')->name('todo.store');
